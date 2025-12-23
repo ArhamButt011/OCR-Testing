c3aadf4bb77990a443a4352b02d14b3d6335cabd
arhambutt011 <arhamhamidbutt@gmail.com>
2025-12-23 11:48:25 +0500

    Work on updateing the base url

--- src/app/context/ApiConfigContext.tsx
+++ src/app/context/ApiConfigContext.tsx
@@ -49,9 +49,8 @@ export const ApiConfigProvider: React.FC<{ children: React.ReactNode }> = ({
           // For remote use below lines
           setOcrApiUrl(`https://kkti3idqzhgqny-8080.proxy.runpod.net/run-ocr`);
           setTestTemplateApiUrl(`https://4lrl8vwxpqp35t-19123-8080.proxy.runpod.net/api/templates/test`);
-          setBaseUrl(`https://fzi6t0m8gas6eb-8080.proxy.runpod.net`);
+          setBaseUrl(`https://glr8hki86gyn0c-3000.proxy.runpod.net`);
           // setBaseUrl(`http://localhost:3000`);
-
         }
       } catch (err) {
         const errorMessage = err instanceof Error ? err.message : "Failed to fetch API configuration";
