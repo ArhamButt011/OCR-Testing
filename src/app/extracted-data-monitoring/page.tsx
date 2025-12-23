0a65f81f30b9b8b1c9a955b656adddb7c77400f4
arhambutt011 <arhamhamidbutt@gmail.com>
2025-12-23 19:16:59 +0500

    Update frontend pod id

--- src/app/extracted-data-monitoring/page.tsx
+++ src/app/extracted-data-monitoring/page.tsx
@@ -478,7 +478,7 @@ const MasterPage = () => {
           // setBaseUrl(`http://${data.secondaryIp}:3000`);
           //for remote use below lines
           setOcrApiUrl(`https://4lrl8vwxpqp35t-8080.proxy.runpod.net/run-ocr`);
-          setBaseUrl(`https://glr8hki86gyn0c-3000.proxy.runpod.net`);
+          setBaseUrl(`https://i66iyo8iouqy19-3000.proxy.runpod.net`);
         }
       } catch (error) {
         console.error("Failed to fetch OCR API URL:", error);
