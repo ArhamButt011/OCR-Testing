11c5a8491dad3d51691d34edc0020b1f972c9bb4
arhambutt011 <arhamhamidbutt@gmail.com>
2026-01-13 12:21:55 +0500

    update store token

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
+import AdminLoginPage from "./admin-login/page";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-2xl font-bold mb-4">Root Page Works!</h1>
-        <a href="/admin-login" className="text-blue-500 underline">
-          Go to Admin Login
-        </a>
-      </div>
-    </div>
+    <>
+      <AdminLoginPage /> 
+    </>
   );
 }
--- src/lib/auth/storeToken.ts
+++ src/lib/auth/storeToken.ts
@@ -1,12 +1,7 @@
-import Cookie from "js-cookie";
+export const storeToken = (username: string, role: string, token: string) => {
+  if (typeof window === "undefined") return;
 
-export const storeToken = (token: string, username: string, role:string) => {
-      Cookie.remove("token");
-      Cookie.remove("username");
-      Cookie.remove("role");
-      const isProduction = process.env.NODE_ENV === "production";
-      const isSecure = isProduction && window.location.protocol === "https:";
-      Cookie.set("token", token, { expires: 1, secure: isSecure, sameSite: "Strict" });
-      Cookie.set("username", username, { expires: 1, secure: isSecure, sameSite: "Strict" });
-      Cookie.set("role", role, { expires: 1, secure: isSecure, sameSite: "Strict" });    
+  localStorage.setItem("username", username);
+  localStorage.setItem("role", role);
+  localStorage.setItem("token", token);
 };
